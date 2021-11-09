6c4c6cdcbfd15cf9b1d45271500d67125b2e1df5
Ilnazfah <ifs3112@gmail.com>
2021-11-09 15:00:59 +0300

    Преобразование даты

---
+++ src/main/java/ru/job4j/grabber/utils/SqlRuDateTimeParser.java
@@ -0,0 +1,37 @@
+package ru.job4j.grabber.utils;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Map;
+
+import static java.util.Map.entry;
+
+public class SqlRuDateTimeParser implements DateTimeParser {
+
+    private static final Map<String, String> MONTHS = Map.ofEntries(
+            entry("янв", "1"), entry("фев", "2"), entry("мар", "3"),
+            entry("апр", "4"), entry("май", "5"), entry("июн", "6"),
+            entry("июл", "7"), entry("авг", "8"), entry("сен", "9"),
+            entry("окт", "10"), entry("ноя", "11"), entry("дек", "12"));
+
+    @Override
+    public LocalDateTime parse(String parse) {
+        LocalDateTime result;
+        DateTimeFormatter dateFormat;
+        if (parse.contains("сегодня")) {
+            int hour = Integer.parseInt(parse.split(" ")[1].split(":")[0]);
+            int minute = Integer.parseInt(parse.split(" ")[1].split(":")[1]);
+            result = LocalDateTime.now().withHour(hour).withMinute(minute);
+        } else if (parse.contains("вчера")) {
+            int hour = Integer.parseInt(parse.split(" ")[1].split(":")[0]);
+            int minute = Integer.parseInt(parse.split(" ")[1].split(":")[1]);
+            result = LocalDateTime.now().minusDays(1).withHour(hour).withMinute(minute);
+        } else {
+            String month = parse.split(" ")[1];
+            String data = parse.replace(month, MONTHS.get(month));
+            dateFormat = DateTimeFormatter.ofPattern("d M yy, HH:mm");
+            result = LocalDateTime.parse(data, dateFormat);
+        }
+        return result;
+    }
+}
--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -4,17 +4,20 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.utils.SqlRuDateTimeParser;
 
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
         Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
         Elements row = doc.select(".postslisttopic");
+        SqlRuDateTimeParser dateTimeParser = new SqlRuDateTimeParser();
         for (Element td : row) {
             Element href = td.child(0);
             System.out.println(href.attr("href"));
             System.out.println(href.text());
             Element parent = td.parent();
-            System.out.println(parent.child(5).text());
+            String date = parent.child(5).text();
+            System.out.println(dateTimeParser.parse(date));
         }
     }
 }
